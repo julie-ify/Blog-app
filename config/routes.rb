0a4f736659e85ae061b95d107e9489b8ca171a95
Juliana Ifionu <julieifionu@gmail.com>
2021-11-30 16:12:37 +0100

    Add show action to User controller and create view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "users#index"
 
-  get 'users/index'
+  get "/users", to: "users#index"
+  get "/users/:id", to: "users#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
