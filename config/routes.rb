e38c9e7890f97e9ee9774c085e6a5231a1a02ac4
Juliana Ifionu <julieifionu@gmail.com>
2021-11-30 16:19:03 +0100

    Create Posts controller with index action

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'posts/index'
   root "users#index"
 
   get "/users", to: "users#index"
   get "/users/:id", to: "users#show"
+
+  get "/users/:user_id/posts", to: "posts#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
