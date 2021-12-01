bd838113ccc1dbaeb77e5ef4b8e9ccd4e66e7e69
Juliana Ifionu <julieifionu@gmail.com>
2021-12-01 18:13:07 +0100

    Use resourses in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   root "users#index"
 
-  get "/users", to: "users#index"
-  get "/users/:id", to: "users#show"
-
-  get "/users/:user_id/posts", to: "posts#index"
-  get "/users/:user_id/posts/:id", to: "posts#show"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: %i[index show] do 
+    resources :posts, only: %i[index show]
+  end
 end
