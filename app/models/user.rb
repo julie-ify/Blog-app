73fdbc54bccc7b6607173f6b751b03a8922d2b42
Juliana Ifionu <julieifionu@gmail.com>
2021-12-06 18:42:03 +0100

    Fix rubocop errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
-
-  has_many :posts, foreign_key: "user_id"
-  has_many :Comments, foreign_key: "users_id"
-  has_many :likes, foreign_key: "users_id"
+  has_many :posts, foreign_key: 'user_id'
+  has_many :Comments, foreign_key: 'users_id'
+  has_many :likes, foreign_key: 'users_id'
 
   def recent_posts
     posts.limit(3).order(created_at: :desc)
