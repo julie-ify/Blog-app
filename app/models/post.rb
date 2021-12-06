fbb69d6258d5b0fb6a0fd745ebdf3e889f38e815
Juliana Ifionu <julieifionu@gmail.com>
2021-12-06 22:02:26 +0100

    Fix rubocop errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
-  belongs_to :author, class_name: "User"
+  belongs_to :author, class_name: 'User'
 
   def update_post_counter
     author.increment!(:posts_counter)
