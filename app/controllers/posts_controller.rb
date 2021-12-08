c0f28a1270fce685fa21669f9c77be8c1eda3a38
Juliana Ifionu <julieifionu@gmail.com>
2021-12-08 16:05:12 +0100

    Create post and like instance

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,13 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @posts = @user.recent_posts
+    @posts = @user.posts.order(created_at: :desc)
   end
 
   def show
     @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:id])
     @comments = @post.comments.all
+    @likes = @post.likes.all
   end
 end
