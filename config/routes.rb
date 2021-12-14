b34301826d170637e32e8fd5a93e60125e0afc9d
Juliana Ifionu <julieifionu@gmail.com>
2021-12-14 14:59:42 +0100

    Add delete route to post

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,12 @@ Rails.application.routes.draw do
   root "users#index"
 
   resources :users, only: %i[index show] do 
-    resources :posts, only: %i[index new create show]
+    resources :posts, only: %i[index new create show destroy]
   end
 
   resources :posts do
     resources :comments, only: %i[create]
     resources :likes, only: %i[create]
   end
+
 end
