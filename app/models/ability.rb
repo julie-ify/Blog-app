89b7387110817ad613236b7510792a4df580ded0
kalunge <muthomititus+github@gmail.com>
2021-12-14 18:10:20 +0100

    Add delete post and unlike post functionality

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -10,6 +10,7 @@ class Ability
     else
       can :read, :all
       can :create, Post
+      can :create, Like
 
       can :destroy, Post do |post|
         post.author_id == user.id
@@ -17,6 +18,10 @@ class Ability
 
       can :destroy, Comment do |comment|
         comment.author_id == user.id
+      end
+
+      can :destroy, Like do |like|
+        like.author_id == user.id
       end
     end
     #
